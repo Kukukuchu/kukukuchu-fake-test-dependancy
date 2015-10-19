4c32f493d62f44b3f65cd05aab57bcf8914f9bd6
Damir Petrač <damir.petrac@gmail.com>
2015-10-20 00:08:26 +0200

    Init project

---
+++ src/app/FAKENugetDemo/QuoteGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAKENugetDemo
+{
+    public class QuoteGenerator
+    {
+        public string GiveMeRandomQuote()
+        {
+            var quotes = File.ReadAllLines(@".\Quotes.txt");
+            var rangen = new Random();
+            int index = rangen.Next(quotes.Length - 1);
+            return string.Format("\"{0}\"", quotes[index]);
+        }
+    }
+}
---
+++ src/test/FAKENugetDemoTest/QuoteGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FAKENugetDemo
+{
+    [TestClass]
+    public class QuoteGeneratorTest
+    {
+        [TestMethod]
+        public void QuoteStartsWithQuotes()
+        {
+            var quotegen = new QuoteGenerator();
+            string quote = quotegen.GiveMeRandomQuote();
+            StringAssert.StartsWith(quote, "\"");
+        }
+
+        [TestMethod]
+        public void QuoteEndsWithQuotes()
+        {
+            var quotegen = new QuoteGenerator();
+            string quote = quotegen.GiveMeRandomQuote();
+            StringAssert.EndsWith(quote, "\"");
+        }
+    }
+}
